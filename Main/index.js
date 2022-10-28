ff640271b55e1689eca5d5632bd453413497bd56
Karina Drummond <k.drummond528@gmail.com>
2022-10-28 11:21:45 -0700

    modified files and code

--- Main/index.js
+++ Main/index.js
@@ -14,11 +14,12 @@ const teamMembers = [];
 
 
 // function for creating manager - inquirer questions
-  // take those questions and create a new Manager with the user provided answers
-  // push that new Manager to the team members array
+// take those questions and create a new Manager with the
+// user provided answers
+// push that new Manager to the team members array
 
-  // follow the same pattern for each type of employee
-  // build a function for them that uses inquirer
+// follow the same pattern for each type of employee
+// build a function for them that uses inquirer
 
 
 // STRUCTURING IT
@@ -26,6 +27,8 @@ const teamMembers = [];
 // start with manager function, since every team needs a manager
 // at the end of manager function, call a createTeam function
 
-// this function would simply ask the user which type of employee they would like to add, based on their choice, run the correesponding function
+// this function would simply ask the user which type of employee they 
+// would like to add, based on their choice, run the correesponding function
 
-// at the end, use fs to write file while sending the team array over to the function you brought in from page-template.js
+// at the end, use fs to write file while sending the team array over to 
+// the function you brought in from page-template.js
